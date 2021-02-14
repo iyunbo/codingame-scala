3944e4e88235f26df96decac72b7ead73d59ba54
Yunbo WANG <w.yunbo@gmail.com>
2021-02-14 09:54:14 +0100

    complete implementation of list flatten

--- src/main/scala/list/Lists.scala
+++ src/main/scala/list/Lists.scala
@@ -3,11 +3,14 @@ package list
 
 object Lists {
 
-  def flatten(xs: List[Any]): List[Any] = xs match {
-    case List() => List()
-    case y :: ys => y match {
-      case z: Int => z :: flatten(ys)
-      case a :: as => a :: flatten(as) ::: flatten(ys)
+  def flatten(xs: List[Any]): List[Any] = {
+    xs match {
+      case List() => List()
+      case List(zs: List[Any]) => flatten(zs)
+      case y :: ys => y match {
+        case i: Int => i :: flatten(ys)
+        case a :: as => a :: flatten(as) ::: flatten(ys)
+      }
     }
   }
 
