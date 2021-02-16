f7549aaaea5a64d1764dcedc03c2079d9f12996c
Yunbo WANG <w.yunbo@gmail.com>
2021-02-16 19:35:10 +0100

    minor update

--- src/main/scala/hardware/MonitorEvaluator.scala
+++ src/main/scala/hardware/MonitorEvaluator.scala
@@ -5,8 +5,8 @@ object MonitorEvaluator {
 
   case class Ratio(width: Double = 16, height: Double = 9)
 
-  def ppi(ratio: Ratio = Ratio(), widthResolution: Int, size: Double): Double = {
-    widthResolution / (size * ratio.height / Math.sqrt(ratio.width * ratio.width + ratio.height * ratio.height))
+  def ppi(ratio: Ratio = Ratio(), heightResolution: Int, size: Double): Double = {
+    heightResolution / (size * ratio.height / Math.sqrt(ratio.width * ratio.width + ratio.height * ratio.height))
   }
 
 }
