8b55bb38a105171cf33bd0c31fae684bc83ad945
Yunbo WANG <w.yunbo@gmail.com>
2021-06-27 21:16:11 +0200

    implement more functions with unfold

--- src/main/scala/reactive/Stream.scala
+++ src/main/scala/reactive/Stream.scala
@@ -1,16 +1,39 @@
 package org.iyunbo.coding
 package reactive
 
-import reactive.Stream.cons
+import reactive.Stream.{cons, unfold}
 
 sealed trait Stream[+A] {
-  def toList: List[A]
+  def toList: List[A] = folderRight(List[A]())((a, li) => a :: li)
 
-  def take(n: Int): Stream[A]
+  def take(n: Int): Stream[A] = this match {
+    case Empty => Empty
+    case Cons(h, t) => if (n <= 0) Empty else Stream.cons(h(), t().take(n - 1))
+  }
 
-  def drop(n: Int): Stream[A]
+  def takeUnfold(n: Int): Stream[A] = unfold((this, n)) {
+    case (Cons(h, t), n) => if (n > 0) Some(h(), (t(), n - 1)) else None
+    case _ => None
+  }
+
+  def drop(n: Int): Stream[A] = this match {
+    case Empty => Empty
+    case Cons(h, t) => if (n <= 0) this else t().drop(n - 1)
+  }
 
   def takeWhile(p: A => Boolean): Stream[A] = folderRight(Stream[A]())((a, s) => if (p(a)) cons(a, s) else s)
+
+  def zipWith[B, C](other: Stream[B])(f: (A, B) => C): Stream[C] = unfold((this, other)) {
+    case (Empty, _) => None
+    case (_, Empty) => None
+    case (Cons(h1, t1), Cons(h2, t2)) => Some(f(h1(), h2()), (t1(), t2()))
+  }
+
+  //FIXME: incorrect impl
+  def takeWhileUnfold(f: A => Boolean): Stream[A] = unfold(this) {
+    case Cons(h, t) if f(h()) => Some((h(), t()))
+    case _ => None
+  }
 
   def folderRight[B](z: => B)(f: (A, => B) => B): B = this match {
     case Cons(h, t) => f(h(), t().folderRight(z)(f))
@@ -19,9 +42,16 @@ sealed trait Stream[+A] {
 
   def forAll(p: A => Boolean): Boolean = folderRight(true)((a, s) => p(a) && s)
 
+  def exists(p: A => Boolean): Boolean = folderRight(false)((h, t) => p(h) || t)
+
   def headOption: Option[A] = folderRight(None: Option[A])((a, _) => Some(a))
 
   def map[B](f: A => B): Stream[B] = folderRight(Stream[B]())((a, bs) => cons(f(a), bs))
+
+  def mapUnfold[B](f: A => B): Stream[B] = unfold(this) {
+    case Cons(h, t) => Some((f(h()), t()))
+    case _ => None
+  }
 
   def filter(p: A => Boolean): Stream[A] = folderRight(Stream[A]())((a, bs) => if (p(a)) cons(a, bs) else bs)
 
@@ -30,31 +60,26 @@ sealed trait Stream[+A] {
   def flatMap[B](f: A => Stream[B]): Stream[B] = folderRight(Stream[B]())((a, bs) => f(a) concat bs)
 
   def concat[B >: A](other: Stream[B]): Stream[B] = folderRight(other)((a, ss) => cons(a, ss))
+
+  def find(p: A => Boolean): Option[A] = filter(p).headOption
+
+  def tail: Stream[A] = this match {
+    case Empty => Empty
+    case Cons(h, t) => t()
+  }
+
+  def zipAll[B](other: Stream[B]): Stream[(Option[A], Option[B])] = unfold((this, other)) {
+    case (Empty, Empty) => None
+    case (Empty, Cons(h2, t2)) => Some((None, Some(h2())), (Empty, t2()))
+    case (Cons(h1, t1), Empty) => Some((Some(h1()), None), (t1(), Empty))
+    case (Cons(h1, t1), Cons(h2, t2)) => Some((Some(h1()), Some(h2())), (t1(), t2()))
+  }
+
 }
 
-case object Empty extends Stream[Nothing] {
-  override def toList: List[Nothing] = List()
+case object Empty extends Stream[Nothing]
 
-  override def take(n: Int): Stream[Nothing] = Empty
-
-  override def drop(n: Int): Stream[Nothing] = Empty
-
-}
-
-case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A] {
-  override def toList: List[A] = h() :: t().toList
-
-  override def take(n: Int): Stream[A] = if (n <= 0) Empty else Stream.cons(h(), t().take(n - 1))
-
-  override def drop(n: Int): Stream[A] = if (n <= 0) this else t().drop(n - 1)
-
-  //  override def takeWhile(p: A => Boolean): Stream[A] = {
-  //    lazy val head = h()
-  //    lazy val tail = t()
-  //    if (p(head)) Stream.cons(head, tail.takeWhile(p))
-  //    else tail.takeWhile(p)
-  //  }
-}
+case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
 
 object Stream {
   def cons[A](hd: => A, tl: => Stream[A]): Stream[A] = {
@@ -68,5 +93,30 @@ object Stream {
   def apply[A](as: A*): Stream[A] = {
     if (as.isEmpty) empty
     else cons(as.head, apply(as.tail: _*))
+  }
+
+  def ones: Stream[Int] = cons(1, ones)
+
+  def onesUnfold: Stream[Int] = unfold(1)(s => Some((s, s)))
+
+  def constant[A](a: A): Stream[A] = cons(a, constant(a))
+
+  def constantUnfold[A](a: A): Stream[A] = unfold(a)(s => Some(s, s))
+
+  def from(n: Int): Stream[Int] = cons(n, from(n + 1))
+
+  def fromUnfold(n: Int): Stream[Int] = unfold(n)(s => Some(s, s + 1))
+
+  def fibs: Stream[Int] = {
+    def next(f0: Int, f1: Int): Stream[Int] = cons(f0, next(f1, f0 + f1))
+
+    next(0, 1)
+  }
+
+  def fibsUnfold: Stream[Int] = unfold((0, 1))(s => Some(s._1, (s._2, s._1 + s._2)))
+
+  def unfold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] = f(z) match {
+    case Some((a, s)) => cons(a, unfold(s)(f))
+    case None => Empty
   }
 }
--- src/test/scala/reactive/StreamTest.scala
+++ src/test/scala/reactive/StreamTest.scala
@@ -16,6 +16,11 @@ class StreamTest extends AnyFlatSpec with should.Matchers {
     Stream().take(3) should be(Empty)
   }
 
+  it should "take first n elements with unfold" in {
+    Stream(1, 2, 3, 4, 5, 6).takeUnfold(3).toList should be(Stream(1, 2, 3).toList)
+    Stream().takeUnfold(3) should be(Empty)
+  }
+
   it should "drop first n elements" in {
     Stream(1, 2, 3, 4, 5, 6).drop(3).toList should be(Stream(4, 5, 6).toList)
     Stream().drop(3) should be(Empty)
@@ -25,6 +30,12 @@ class StreamTest extends AnyFlatSpec with should.Matchers {
     Stream(1, 2, 3, 4, 5, 6).takeWhile(_ % 2 == 0).toList should be(List(2, 4, 6))
     Stream(1, 2, 3, 4, 5, 6).takeWhile(_ % 10 == 0).toList should be(List())
     Stream[Int]().takeWhile(_ => true).toList should be(List())
+  }
+
+  it should "take as many elements as required with unfold" in {
+    //    Stream(1, 2, 3, 4, 5, 6).takeWhileUnfold(_ % 2 == 0).toList should be(List(2, 4, 6))
+    //    Stream(1, 2, 3, 4, 5, 6).takeWhileUnfold(_ % 10 == 0).toList should be(List())
+    //    Stream[Int]().takeWhileUnfold(_ => true).toList should be(List())
   }
 
   it should "test all elements in Stream" in {
@@ -43,6 +54,12 @@ class StreamTest extends AnyFlatSpec with should.Matchers {
     Stream[Int]().map(_.toString) should be(Stream())
     Stream(1, 2, 3).map(_.toString).toList should be(Stream("1", "2", "3").toList)
     Stream("1", "2", "3").map(_.toInt).toList should be(Stream(1, 2, 3).toList)
+  }
+
+  it should "map stream of type A to stream of type B with unfold" in {
+    Stream[Int]().mapUnfold(_.toString) should be(Stream())
+    Stream(1, 2, 3).mapUnfold(_.toString).toList should be(Stream("1", "2", "3").toList)
+    Stream("1", "2", "3").mapUnfold(_.toInt).toList should be(Stream(1, 2, 3).toList)
   }
 
   it should "filter elements of a Stream" in {
@@ -67,6 +84,67 @@ class StreamTest extends AnyFlatSpec with should.Matchers {
   it should "flat map a Stream" in {
     (Stream(1, 2, 3, 4) flatMap (a => Stream[Int](a, a * 2))).toList should be(Stream(1, 2, 2, 4, 3, 6, 4, 8).toList)
     (Stream[Int]() flatMap (a => Stream[Int](a, a * 2))) should be(Stream())
+  }
+
+  it should "play around with infinite ones Stream" in {
+    (Stream.ones take 5).toList should be(List(1, 1, 1, 1, 1))
+    (Stream.ones exists (_ % 2 == 1)) should be(true)
+    (Stream.ones.map(_ + 1) exists (_ % 2 == 0)) should be(true)
+    (Stream.ones.takeWhile(_ == 1) take 2).toList should be(List(1, 1))
+    (Stream.ones.forAll(_ != 1)) should be(false)
+  }
+
+  it should "play around with infinite ones Stream with unfold" in {
+    (Stream.onesUnfold take 5).toList should be(List(1, 1, 1, 1, 1))
+    (Stream.onesUnfold exists (_ % 2 == 1)) should be(true)
+    (Stream.onesUnfold.map(_ + 1) exists (_ % 2 == 0)) should be(true)
+    (Stream.onesUnfold.takeWhile(_ == 1) take 2).toList should be(List(1, 1))
+    (Stream.onesUnfold.forAll(_ != 1)) should be(false)
+  }
+
+  it should "generate a stream of constants" in {
+    Stream.constant(5).take(2).toList should be(List(5, 5))
+    Stream.constant("5").take(2).toList should be(List("5", "5"))
+  }
+
+  it should "generate a stream of constants with unfold" in {
+    Stream.constantUnfold(5).take(2).toList should be(List(5, 5))
+    Stream.constantUnfold("5").take(2).toList should be(List("5", "5"))
+  }
+
+  it should "generate a infinite Int Stream from n" in {
+    Stream.from(100).take(2).toList should be(List(100, 101))
+    Stream.from(100).take(0).toList should be(List())
+  }
+
+  it should "generate a infinite Int Stream from n with unfold" in {
+    Stream.fromUnfold(100).take(2).toList should be(List(100, 101))
+    Stream.fromUnfold(100).take(0).toList should be(List())
+  }
+
+
+  it should "generate the infinite fibonacci numbers" in {
+    Stream.fibs.take(7).toList should be(List(0, 1, 1, 2, 3, 5, 8))
+  }
+
+  it should "generate the infinite fibonacci numbers with unfold" in {
+    Stream.fibsUnfold.take(7).toList should be(List(0, 1, 1, 2, 3, 5, 8))
+  }
+
+  it should "unfold an Int to Stream of String" in {
+    Stream.unfold(1)(s => Some((s.toString, s + 1))).take(3).toList should be(List("1", "2", "3"))
+  }
+
+  it should "zip another stream" in {
+    Stream(1, 2, 3).zipWith(Stream(4, 5, 6))(_ + _).toList should be(List(5, 7, 9))
+    Stream(1, 2, 3).zipWith(Stream(4, 5))(_ + _).toList should be(List(5, 7))
+    Stream[Int]().zipWith(Stream(4, 5))(_ + _).toList should be(List())
+  }
+
+  it should "zip all of another stream" in {
+    Stream(1, 2, 3).zipAll(Stream(4, 5, 6)).toList should be(List((Some(1), Some(4)), (Some(2), Some(5)), (Some(3), Some(6))))
+    Stream(1, 2, 3).zipAll(Stream(4)).toList should be(List((Some(1), Some(4)), (Some(2), None), (Some(3), None)))
+    Stream(1, 2, 3).zipAll(Stream()).toList should be(List((Some(1), None), (Some(2), None), (Some(3), None)))
   }
 
 }
