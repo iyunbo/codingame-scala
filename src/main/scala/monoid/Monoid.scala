07267995f976760b4b267e529635b22b36c8182e
Yunbo WANG <yunbo.wang@contentsquare.com>
2021-12-27 21:41:05 +0100

    monad in progress

--- src/main/scala/monoid/Monoid.scala
+++ src/main/scala/monoid/Monoid.scala
@@ -1,12 +1,14 @@
 package org.iyunbo.coding
 package monoid
 
+import pbt.{Gen, Prop}
+
 object Monoid {
 
   def compose[A](f1: A => A, f2: A => A): A => A = a => f1(f2(a))
 
-
-  def verifyWithInts(predicate: Int => Boolean): Unit = for (v <- 1 to 999) assert(predicate(v))
+  def verifyWithInts(predicate: Int => Boolean): Unit = for (v <- 1 to 999)
+    assert(predicate(v))
 
   type Func = Int => Int
 
@@ -16,7 +18,6 @@ object Monoid {
   val h: Func = (x: Int) => x + 3
 
   def experiment(): Unit = {
-
 
     // This is our neutral element, a function which does nothing
     val id = identity[Int] _
@@ -43,7 +44,6 @@ object Monoid {
 
   def experimentMonoid(): Unit = {
 
-
     // identity element
     val id = identity[Func] _
 
@@ -55,8 +55,64 @@ object Monoid {
 
     val fh_g = op(op(f, h), g)
 
-
     verifyWithInts(a => g_fh(a) == fh_g(a))
   }
 
+  def monoidLaws[A](m: Monoid[A], gen: Gen[A]): Prop = {
+    Prop.forAll(gen) { a =>
+      m.op(a, m.zero) == a && m.op(m.zero, a) == a
+    }
+
+    Prop.forAll(gen.listOf(Gen.unit(3))) { values =>
+      {
+        val a = values.toIndexedSeq
+        m.op(a(0), m.op(a(1), a(2))) == m.op(m.op(a(0), a(1)), a(2))
+      }
+    }
+  }
+
+  val wcMonoid: Monoid[WC] = new Monoid[WC] {
+    override def op(a1: WC, a2: WC): WC = (a1, a2) match {
+      case (Stub(s1), Stub(s2)) => Stub(s1 + s2)
+      case (Stub(s1), Part(l, n, r)) => Part(s1+l, n, r)
+      case (Part(l,n, r), Stub(s2)) => Part(l, n, r + s2)
+      case (Part(l1, n1, r1), Part(l2, n2, r2)) => Part(l1, n1 + n2, r1 + l2)
+    }
+
+    override def zero: WC = Stub("")
+
+//    def merge(s1: String, s2: String): WC = if(s1.endsWith(" ") || s2.startsWith(" ")) Part(s1, )
+  }
+
 }
+
+trait Monoid[A] {
+  self =>
+  def op(a1: A, a2: A): A
+  def zero: A
+
+  def optionMonoid: Monoid[Option[A]] = new Monoid[Option[A]] {
+    override def op(a1: Option[A], a2: Option[A]): Option[A] = (a1, a2) match {
+      case (Some(v1), Some(v2)) => Some(self.op(v1, v2))
+      case (_, _)               => None
+    }
+
+    override def zero: Option[A] = None
+  }
+
+  def edoMonoid: Monoid[A => A] = new Monoid[A => A] {
+    override def op(a1: A => A, a2: A => A): A => A = a => self.op(a1(a), a2(a))
+
+    override def zero: A => A = a => a
+  }
+
+  def concatenate(as: List[A]): A = as.foldLeft(zero)(op)
+
+  def foldMap[B](as: List[A], m: Monoid[B])(f: A => B): B =
+    m.concatenate(as map f)
+}
+
+sealed trait WC
+case class Stub(chars: String) extends WC
+case class Part(lStub: String, words: Int, rStub: String) extends WC
+
--- src/test/scala/monoid/MonoidTest.scala
+++ src/test/scala/monoid/MonoidTest.scala
@@ -1,6 +1,8 @@
 package org.iyunbo.coding
 package monoid
 
+import org.iyunbo.coding.monoid.Monoid.monoidLaws
+import org.iyunbo.coding.pbt.{Gen, Prop}
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should
 
@@ -10,6 +12,41 @@ class MonoidTest extends AnyFlatSpec with should.Matchers {
     Monoid.experiment()
     Monoid.experiment2()
     Monoid.experimentMonoid()
+  }
+
+  it should "create basic Monoids" in {
+    val intAddition: Monoid[Int] = new Monoid[Int] {
+      override def op(a1: Int, a2: Int): Int = a1 + a2
+
+      override def zero: Int = 0
+    }
+
+    val intMultiplication: Monoid[Int] = new Monoid[Int] {
+      override def op(a1: Int, a2: Int): Int = a1 * a2
+
+      override def zero: Int = 1
+    }
+
+    val booleanOr: Monoid[Boolean] = new Monoid[Boolean] {
+      override def op(a1: Boolean, a2: Boolean): Boolean = a1 || a2
+
+      override def zero: Boolean = false
+    }
+
+    val booleanAnd: Monoid[Boolean] = new Monoid[Boolean] {
+      override def op(a1: Boolean, a2: Boolean): Boolean = a1 && a2
+
+      override def zero: Boolean = true
+    }
+
+    List(intAddition, intMultiplication).foreach(monoid =>
+      Prop.run(monoidLaws(monoid, Gen.int))
+    )
+
+    List(booleanOr, booleanAnd).foreach(monoid =>
+      Prop.run(monoidLaws(monoid, Gen.boolean))
+    )
+
   }
 
 }
